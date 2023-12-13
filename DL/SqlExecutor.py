523daa49e711caa19d6f79f101724227731cf72d
yotamlevit <yotam.levit@gmail.com>
2023-12-13 21:33:57 +0200

    Added update function and order by option

--- DL/SqlExecutor.py
+++ DL/SqlExecutor.py
@@ -11,10 +11,12 @@ class SqlExecutor:
             cls._instance.cursor = cls._instance.connection.cursor()
         return cls._instance
 
-    def select(self, table, columns='*', condition='', params=None):
+    def select(self, table, columns='*', condition='', order_by='', params=None):
         query = f"SELECT {columns} FROM {table}"
         if condition:
             query += f" WHERE {condition}"
+        if order_by:
+            query += f" ORDER BY {order_by}"
         try:
             if params:
                 self.cursor.execute(query, params)
@@ -27,23 +29,25 @@ class SqlExecutor:
 
     def insert(self, table, columns, values):
         query = f"INSERT INTO {table} ({', '.join(columns)}) VALUES ({', '.join(['?'] * len(values))})"
-        print(query)
         try:
             self.cursor.execute(query, values)
             self.connection.commit()
         except sqlite3.Error as e:
             print(f"Error executing INSERT query: {e}")
 
-    def update(self, table, values, condition='', params=None):
-        set_values = ', '.join([f"{col} = ?" for col in values])
-        query = f"UPDATE {table} SET {set_values}"
+    def update(self, table, update_values: dict, condition='', params=None):
+        set_keys = ', '.join([f'{key}=?' for key in update_values.keys()])
+        set_values = list(update_values.values())
+
+        query = f"UPDATE {table} SET {set_keys}"
         if condition:
             query += f" WHERE {condition}"
+        print(query)
         try:
             if params:
                 self.cursor.execute(query, params)
             else:
-                self.cursor.execute(query)
+                self.cursor.execute(query, set_values)
             self.connection.commit()
         except sqlite3.Error as e:
             print(f"Error executing UPDATE query: {e}")
