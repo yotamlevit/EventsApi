42b0a7a090fd4cc92b1ffe9f17041d4b2dc8896e
yotamlevit <yotam.levit@gmail.com>
2023-12-13 21:47:06 +0200

    Add docs

--- App/App.py
+++ App/App.py
@@ -3,7 +3,42 @@ from .Routers import initialize_routers
 from .Dependencies import authenticate, bl_factory
 
 
+tags_metadata = [
+    {
+        "name": "Event API",
+        "description": "All Event related operations",
+    },
+    {
+        "name": "Users API",
+        "description": "All Users related operations",
+    },
+]
+
+
+description = """
+RalfaBet Backend API 🔥🔥🔥
+
+## Items
+
+You can **read items**.
+
+## For Users
+
+You will be able to:
+
+### Events:
+* **Get all events**
+* **Get a specific event**
+* **Create a new event**
+* **Update an existing event**
+* **Delete an event**
+
+### Users
+* **Create users** (_not implemented_).
+* **Read users** (_not implemented_).
+"""
+
 def app() -> FastAPI:
-    app = FastAPI(dependencies=[Depends(authenticate)])
+    app = FastAPI(description=description , openapi_tags=tags_metadata, dependencies=[Depends(authenticate)])
     initialize_routers(app)
     return app
