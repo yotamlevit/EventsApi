1dc5a88631f2af4e5eb3e18d56bb5ba76cb2cf46
yotamlevit <yotam.levit@gmail.com>
2023-12-13 21:47:22 +0200

    Change to json response

--- App/Routers/Events.py
+++ App/Routers/Events.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, Depends, Body
+from fastapi.responses import JSONResponse
 from App.Dependencies import bl_factory
 from BL.DTO import EventDTO
 
@@ -10,35 +11,42 @@ events_api = APIRouter(
 
 @events_api.get("")
 def get_events(event_manager = Depends(bl_factory)):
-    return event_manager.get_events()
+    content, status_code = event_manager.get_events()
+    return JSONResponse(status_code=status_code, content=content)
 
 
 @events_api.get("/{event_id}")
 def get_event(event_id: str, event_manager = Depends(bl_factory)):
-    return event_manager.get_event(event_id)
+    content, status_code = event_manager.get_event(event_id)
+    return JSONResponse(status_code=status_code, content=content)
 
 
 @events_api.delete("/{event_id}")
 def delete_event(event_id: str, event_manager = Depends(bl_factory)):
-    return event_manager.delete_event_by_id(event_id)
+    content, status_code = event_manager.delete_event_by_id(event_id)
+    return JSONResponse(status_code=status_code, content=content)
 
 @events_api.put("/{event_id}")
 def update_event(event_id: str, event_data : dict = Body(), event_manager = Depends(bl_factory)):
-    return event_manager.update_event(event_id, event_data)
+    content, status_code = event_manager.update_event(event_id, event_data)
+    return JSONResponse(status_code=status_code, content=content)
 
 
 @events_api.post("")
 def create_event(event_data : EventDTO = Body(), event_manager = Depends(bl_factory)):
-    return event_manager.create_event(event_data)
+    content, status_code = event_manager.create_event(event_data)
+    return JSONResponse(status_code=status_code, content=content)
 
 
 @events_api.get('/location/{event_location}')
 def get_event_by_location(event_location, event_manager = Depends(bl_factory)):
-    return event_manager.get_event_by_location(event_location)
+    content, status_code = event_manager.get_event_by_location(event_location)
+    return JSONResponse(status_code=status_code, content=content)
 
 @events_api.get('/sort/{sort_key}')
 def get_event_by_sort_key(sort_key, event_manager = Depends(bl_factory)):
-    return event_manager.get_event_by_sort_key(sort_key)
+    content, status_code = event_manager.get_event_by_sort_key(sort_key)
+    return JSONResponse(status_code=status_code, content=content)
 
 @events_api.get("/{event_name}")
 def get_event_by_name(event_name: str, event_manager = Depends(bl_factory)):
