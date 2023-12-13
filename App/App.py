2a614baa43a42662d0ee0dac2af895381ce1b4a4
yotamlevit <yotam.levit@gmail.com>
2023-12-13 21:32:07 +0200

    Add basic atuh to all routes

--- App/App.py
+++ App/App.py
@@ -1,8 +1,9 @@
-from fastapi import FastAPI
+from fastapi import FastAPI, Depends
 from .Routers import initialize_routers
+from .Dependencies import authenticate, bl_factory
 
 
 def app() -> FastAPI:
-    app = FastAPI()
+    app = FastAPI(dependencies=[Depends(authenticate)])
     initialize_routers(app)
     return app
