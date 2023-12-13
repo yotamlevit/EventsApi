e5e5b60d4112022362b19892f4b356fa0df31171
yotamlevit <yotam.levit@gmail.com>
2023-12-13 21:35:29 +0200

    Added all events apis

--- App/Routers/Events.py
+++ App/Routers/Events.py
@@ -1,14 +1,7 @@
 from fastapi import APIRouter, Depends, Body
-from App.Dependencies import authenticate
+from App.Dependencies import bl_factory
 from BL.DTO import EventDTO
-from BL.Events import EventManager
 
-
-from DL.EventDB import EventDB
-event_db = EventDB()
-
-# Create an instance of EventManager
-event_manager = EventManager(event_db)
 events_api = APIRouter(
     prefix="/events",
     tags=["Event API"]
@@ -16,28 +9,41 @@ events_api = APIRouter(
 
 
 @events_api.get("")
-def get_events(auth = Depends(authenticate)):
-    pass
+def get_events(event_manager = Depends(bl_factory)):
+    return event_manager.get_events()
 
 
-@events_api.get("/{event_name}")
-def get_event(event_name: str, auth = Depends(authenticate)):
-    pass
-
-@events_api.delete("/{event_name}")
-def delete_event(event_name: str):
-    pass
+@events_api.get("/{event_id}")
+def get_event(event_id: str, event_manager = Depends(bl_factory)):
+    return event_manager.get_event(event_id)
 
 
 @events_api.delete("/{event_id}")
-def delete_event(event_id: str):
-    pass
+def delete_event(event_id: str, event_manager = Depends(bl_factory)):
+    return event_manager.delete_event_by_id(event_id)
 
-@events_api.put("/{event_name}")
-def update_event(event_name: str, event_data : EventDTO = Body()):
-    pass
+@events_api.put("/{event_id}")
+def update_event(event_id: str, event_data : dict = Body(), event_manager = Depends(bl_factory)):
+    return event_manager.update_event(event_id, event_data)
 
 
 @events_api.post("")
-def create_event(event_data : EventDTO = Body()):
-    event_manager.create_event(event_data)
+def create_event(event_data : EventDTO = Body(), event_manager = Depends(bl_factory)):
+    return event_manager.create_event(event_data)
+
+
+@events_api.get('/location/{event_location}')
+def get_event_by_location(event_location, event_manager = Depends(bl_factory)):
+    return event_manager.get_event_by_location(event_location)
+
+@events_api.get('/sort/{sort_key}')
+def get_event_by_sort_key(sort_key, event_manager = Depends(bl_factory)):
+    return event_manager.get_event_by_sort_key(sort_key)
+
+@events_api.get("/{event_name}")
+def get_event_by_name(event_name: str, event_manager = Depends(bl_factory)):
+    pass
+
+@events_api.delete("/name/{event_name}")
+def delete_event_by_name(event_name: str, event_manager = Depends(bl_factory)):
+    pass
