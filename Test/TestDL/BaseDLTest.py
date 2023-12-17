33e209591f17877e5a7f681a0ceefa2e44c16437
yotamlevit <yotam.levit@gmail.com>
2023-12-17 22:06:12 +0200

    Change setup_class to regular function

--- Test/TestDL/BaseDLTest.py
+++ Test/TestDL/BaseDLTest.py
@@ -1,22 +1,28 @@
 from DL.SqlExecutor import SqlExecutor
 
+
 class BaseDLTest:
-    @classmethod
-    def setup_class(cls, table_name, table_columns , table_values, create_table_query):
-        cls.table_name = table_name
-        cls.table_columns = table_columns
-        cls.table_values = table_values
-        cls.sql_executor = cls.__init_sql_executor(cls, create_table_query)
-        cls.__insert_test_values(cls)
-
-    def __init_sql_executor(self, create_table_query):
-        # Create the test table
-        sql_executor = SqlExecutor(db_name=':memory:')
-        sql_executor.cursor.execute(create_table_query)
-
-        return sql_executor
+    def setup_class(self, table_name, table_columns , table_values, create_table_query):
+        self.table_name = table_name
+        self.table_columns = table_columns
+        self.table_values = table_values
+        self.sql_executor = SqlExecutor(db_name=':memory:')
+        self.__create_test_table(self, create_table_query=create_table_query)
 
     def __insert_test_values(self):
         for test_value in self.table_values:
             query = f'''INSERT INTO {self.table_name} ({', '.join(self.table_columns)}) VALUES ({', '.join(['?'] * len(self.table_columns))})'''
             self.sql_executor.cursor.execute(query, test_value)
+
+    def __check_table_exist(self):
+        self.sql_executor.cursor.execute('SELECT name FROM sqlite_master WHERE type = "table"')
+        tables = self.sql_executor.cursor.fetchall()
+        tables = [table[0] for table in tables]
+        return self.table_name in tables
+
+    def __create_test_table(self, create_table_query):
+        if not self.__check_table_exist(self):
+            self.sql_executor.cursor.execute(create_table_query)
+        else:
+            self.sql_executor.cursor.execute(f'DELETE FROM {self.table_name}')
+        self.__insert_test_values(self)
