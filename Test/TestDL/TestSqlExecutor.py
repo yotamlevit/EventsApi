ac02b390c147e61675b590d591a3f9091f6a40cb
yotamlevit <yotam.levit@gmail.com>
2023-12-17 15:20:01 +0200

    Finished sql extractor unit test

--- Test/TestDL/TestSqlExecutor.py
+++ Test/TestDL/TestSqlExecutor.py
@@ -1,57 +1,67 @@
-from Test.TestDL.Arrange import table_metadata, unpack_test_values, sql_executor
+from .BaseDLTest import BaseDLTest
+
+class TestSqlExecutor(BaseDLTest):
+    @classmethod
+    def setup_class(cls):
+        table_name = 'test_table'
+        table_columns = ["name", "value1"]
+        table_values = [["select_test", "value1"], ["update_test", "value2"], ["delete_test", "value3"]]
+        create_table_query = f'''CREATE TABLE {table_name} (id INTEGER, name TEXT, value1 TEXT, PRIMARY KEY("id" AUTOINCREMENT))'''
+        super().setup_class(table_name=table_name, table_columns=table_columns, table_values=table_values, create_table_query=create_table_query)
+
+    def test_select(self):
+        test_name = 'select_test'
+        condition = f'name="{test_name}"'
 
 
-def test_select(sql_executor, table_metadata):
-    table_name, id, name = unpack_test_values(table_metadata, 'select_test')
-    condition = f'id = {id}'
+        # Perform a select operation using SqlExecutor
+        result_name = self.sql_executor.select(self.table_name, columns='name', condition=condition)
+
+        result = self.sql_executor.select(self.table_name)
+        print(result)
+
+        assert result_name == [(test_name,)]
 
 
-    # Perform a select operation using SqlExecutor
-    result_name = sql_executor.select(table_name, columns='name', condition=condition)
-    result = sql_executor.select(table_name, columns='id, name', condition=condition)
+    def test_update(self):
+        name = 'update_test'
+        condition = f'name = "{name}"'
+        updated_name = 'UpdatedName'
 
-    assert result_name == [(name,)]
-    assert result == [(id, name)]
+        # Perform an update operation using SqlExecutor
+        update_values = {'name': updated_name}
+        self.sql_executor.update(self.table_name, update_values, condition=condition)
+        condition = f'name = "{updated_name}"'
 
+        # Check if the data was updated correctly
+        self.sql_executor.cursor.execute(f'SELECT name FROM {self.table_name} WHERE {condition}')
+        result = self.sql_executor.cursor.fetchall()
 
-def test_update(sql_executor, table_metadata):
-    table_name, id, name = unpack_test_values(table_metadata, 'update_test')
-    condition = f'id = {id}'
-    updated_name = 'UpdatedName'
-
-    # Perform an update operation using SqlExecutor
-    update_values = {'name': updated_name}
-    sql_executor.update(table_name, update_values, condition=condition)
-
-    # Check if the data was updated correctly
-    sql_executor.cursor.execute(f'SELECT name FROM {table_name} WHERE {condition}')
-    result = sql_executor.cursor.fetchall()
-
-    assert result == [(updated_name,)]
+        assert result == [(updated_name,)]
 
 
-def test_insert(sql_executor, table_metadata):
-    values = [9999, 'insert_test', "date", "team1", "team2", "location", 0, "insertion_time"]
+    def test_insert(self):
+        values = ['insert_test', "value"]
 
-    # Perform the insert operation using SqlExecutor
-    sql_executor.insert(table_metadata['table_name'], table_metadata['columns'], values)
+        # Perform the insert operation using SqlExecutor
+        self.sql_executor.insert(self.table_name, self.table_columns, values)
 
-    # Check if the data was inserted correctly
-    sql_executor.cursor.execute(f'SELECT * FROM {table_metadata['table_name']} WHERE id=9999')
-    result = sql_executor.cursor.fetchall()
+        # Check if the data was inserted correctly
+        self.sql_executor.cursor.execute(f'SELECT * FROM {self.table_name} WHERE name="insert_test"')
+        result = self.sql_executor.cursor.fetchall()
 
-    assert result == [tuple(values)]
+        assert result[0][1:] == tuple(values)
 
 
-def test_delete(sql_executor, table_metadata):
-    table_name, id, name = unpack_test_values(table_metadata, 'delete_test')
-    condition = f'id = {id}'
+    def test_delete(self):
+        name = 'delete_test'
+        condition = f'name = "{name}"'
 
-    # Perform an update operation using SqlExecutor
-    sql_executor.delete(table_name, condition=condition)
+        # Perform an update operation using SqlExecutor
+        self.sql_executor.delete(self.table_name, condition=condition)
 
-    # Check if the data was updated correctly
-    sql_executor.cursor.execute(f'SELECT name FROM {table_name} WHERE {condition}')
-    result = sql_executor.cursor.fetchall()
+        # Check if the data was updated correctly
+        self.sql_executor.cursor.execute(f'SELECT name FROM {self.table_name} WHERE {condition}')
+        result = self.sql_executor.cursor.fetchall()
 
-    assert result == []
+        assert result == []
