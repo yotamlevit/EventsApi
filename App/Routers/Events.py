a5c593e1869cce27af2060e7ef6098b263136b15
yotamlevit <yotam.levit@gmail.com>
2023-12-17 22:31:23 +0200

    Fix typo

--- App/Routers/Events.py
+++ App/Routers/Events.py
@@ -41,7 +41,7 @@ def create_event(event_data : EventDTO = Body(), event_manager = Depends(bl_fact
 
 @events_api.get('/location/{event_location}')
 def get_event_by_location(event_location: str, event_manager = Depends(bl_factory)):
-    content, status_code = event_manager.qwget_event_by_location(event_location)
+    content, status_code = event_manager.get_event_by_location(event_location)
     return JSONResponse(status_code=status_code, content=content)
 
 @events_api.get('/sort/{sort_key}')
